4a59f4d8e32999e4b4e3e760d04fc07eb2bf4790
ManuelFas <manuelfasruiz@gmail.com>
2025-12-18 12:08:29 +0100

    Added the manual_seed(42) so train/val -> evaluation loaders are the same

--- Otto_TRACE/utils/SplitData.py
+++ Otto_TRACE/utils/SplitData.py
@@ -6,15 +6,17 @@ sys.path.append(PROJECT_ROOT)
 from dataset.otto_trace import TraceOttoDataSet
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
+import torch
 
 def split_data_Train_Val_Test(data_set : TraceOttoDataSet, batch_size: int = 32) -> Tuple[DataLoader, DataLoader, DataLoader]:
     
+    generator = torch.Generator().manual_seed(42)
     # Data splitting train/test/val
     dataset_size = len(data_set)
     train_size = int(0.80 * dataset_size)
     val_size = int(0.10 * dataset_size)
     test_size = dataset_size - train_size - val_size
-    train_data, val_data, test_data = random_split(dataset=data_set, lengths=[train_size, val_size, test_size])
+    train_data, val_data, test_data = random_split(dataset=data_set, lengths=[train_size, val_size, test_size],generator=generator)
     
     #TRAIN SET
     train_loader = DataLoader(
