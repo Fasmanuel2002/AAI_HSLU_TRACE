7d3ae178587c28a37552a86237677964aae81320
ManuelFas <manuelfasruiz@gmail.com>
2025-12-18 12:07:44 +0100

    Comment and Changed the Logic -> F1 added

--- Otto_TRACE/training_version/main_GpuHubjupyter_OneTask.py
+++ Otto_TRACE/training_version/main_GpuHubjupyter_OneTask.py
@@ -15,17 +15,18 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-        #DataSet    
+    
+    #DataSet    
     dataset_processed = TraceOttoDataSet(
         file_name='train.jsonl',
         input_seq_len=64,
         min_timestamps_per_sample=16
     )
     
-    
-
+    #Split the Data into Training_loader, Validation_loader and test_loaders
     train_loader, validation_loader, test_loader = split_data_Train_Val_Test(dataset_processed)
     
+    #calling the max aid and type for combating the Out of Range Error -> Learning Embeddings
     max_aid = max(
         session[0]["aid"].max().item()
         for session in dataset_processed
@@ -34,9 +35,10 @@ def main():
         session[0]["type"].max().item()
         for session in dataset_processed
     )
-
     num_embeddings_aid = max_aid + 1  
     num_embeddings_event_type = max_type + 1
+    
+    #Initialize the Model TRACE(Model Architecture TRACE paper Part 2.3)
     trace_model = TRACE(
         num_embeddings_aid=num_embeddings_aid,
         num_embeddings_event_type=num_embeddings_event_type,
@@ -46,17 +48,14 @@ def main():
     
     trace_model = trace_model.to(device)
     optimizer = optim.AdamW(trace_model.parameters(), lr=3e-5, weight_decay=1e-6)
-    early_stopping = EarlyStopping(
-    patience=6,
-    min_delta=1e-3,
-        mode="max",
-        path="best_Check_TRACE_PD1_model.pt"
-        )
+    early_stopping = EarlyStopping(patience=6,min_delta=1e-3,mode="max",path="best_Check_TRACE_PD1_model.pt")
     num_epochs = 40
 
     #Summary Writer for tensorBoard
     tensor_board_writer = SummaryWriter(log_dir=f"runs/Final/PD1_MODEL_SingleTask_version2")
+    
     print("Started the Training")
+    
     #Figthing Data Imbalanced
     pos_weight = torch.tensor([4.0], device=device)
     criterion = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
@@ -67,44 +66,46 @@ def main():
         all_train_y_true = []
         all_train_y_pred = []
         
-        
         #F1 Score validation
         all_val_y_true = []
         all_val_y_pred = []
         
         
         # -------------------------------TRAINING ---------------------------
+        #Initializing the training variables
         trace_model.train()
         epoch_loss = 0.0
         correct_train_PD1 = 0
         total_train_PD1 = 0
         
         for inputs_train, targets_train in train_loader:
+            
             label_train_PD1 = targets_train["PD1"].unsqueeze(1).to(device)
+            #Changing the Inputs -> to have GPU for JupyterGPUHub
             inputs_train = {
                 k: v.to(device)
                     for k, v in inputs_train.items()
                 }
+            #Calculation of the timestamps(Feature Engineer Trace Paper Part 2.2)
             delta_elapsed = get_elapsed_feature(inputs_train["timestamps"]).to(device)
             delta_between = get_between_features(inputs_train["timestamps"]).to(device)
             
+            #Predictions of the model
             logits_train = trace_model(
                     inputs_train["aid"],
                     inputs_train["type"],
                     delta_elapsed,
                     delta_between
                 )
-
+            
+            #Calculation loss for Training using BCEWithLogitsLoss
             loss_training = criterion(logits_train,label_train_PD1.float())
-            
             optimizer.zero_grad()
-            
             loss_training.backward()
-                
             optimizer.step()
-
             epoch_loss += loss_training.item()
-                # ============ PD1 ============
+            
+            # ============ PD1(Prediction, calculation of Accuracy) ============
             probs_PD1 = torch.sigmoid(logits_train)
             preds_PD1 = (probs_PD1 >= 0.5).float()
             correct_train_PD1 += (preds_PD1 == label_train_PD1).sum().item()
@@ -114,7 +115,7 @@ def main():
             all_train_y_true.append(label_train_PD1.detach().cpu())
             all_train_y_pred.append(preds_PD1.detach().cpu())
 
-
+        #Training Loss and Accuracy 
         train_loss = epoch_loss / len(train_loader)
         train_acc_PD1 = correct_train_PD1 / max(total_train_PD1, 1)
         
@@ -123,13 +124,14 @@ def main():
         all_train_y_pred = torch.cat(all_train_y_pred).numpy()
         train_f1_PD1 = f1_score(all_train_y_true, all_train_y_pred, zero_division=0)
         
+        #TensorBoard Writing
         tensor_board_writer.add_scalar("Train/F1_PD1", train_f1_PD1, epoch)
-
         tensor_board_writer.add_scalar("Training/Loss", train_loss, epoch)
-
         tensor_board_writer.add_scalar("Train/Acc_PD1", train_acc_PD1, epoch)
 
-            # -------------------------------VALIDATION---------------------------
+
+        # -------------------------------VALIDATION---------------------------
+        #Initializing the validation variables    
         trace_model.eval()
         val_loss = 0.0
         correct_val_PD1 = 0
@@ -141,30 +143,28 @@ def main():
                     
                 label_val_PD1 = targets_val["PD1"].unsqueeze(1).to(device)
 
+                #Changing the Inputs -> to have GPU for JupyterGPUHub
                 inputs_val = {
                     k: v.to(device)
                     for k, v in inputs_val.items()
                 }
-
+                #Calculation of the timestamps(Part of Feature Engineer Trace Paper part 2.2)
                 delta_elapsed = get_elapsed_feature(inputs_val["timestamps"]).to(device)
                 delta_between = get_between_features(inputs_val["timestamps"]).to(device)
 
+                #Predictions of the model
                 logits_val = trace_model(
                     inputs_val["aid"],
                     inputs_val["type"],
                     delta_elapsed,
                     delta_between
                     )
-
-                
-
-                
-
+            
+                #Calculation loss for validation using BCEWithLogitsLoss
                 loss_validation = criterion(logits_val,label_val_PD1.float())
-                
                 val_loss += loss_validation.item()
 
-                    #PD1 Debuggin
+                # ============ PD1(Prediction, calculation of Accuracy) ============
                 probs_PD1_val = torch.sigmoid(logits_val)
                 preds_PD1_val = (probs_PD1_val >= 0.5).float()
                 correct_val_PD1 += (preds_PD1_val == label_val_PD1).sum().item()
@@ -180,10 +180,12 @@ def main():
         all_val_y_pred = torch.cat(all_val_y_pred).numpy()
         val_f1_PD1 = f1_score(all_val_y_true, all_val_y_pred, zero_division=0)
         
+        
+        #Validation Loss and Accuracy 
         val_loss /= len(validation_loader)
-
-            
         val_acc_PD1 = correct_val_PD1 / max(total_val_PD1, 1)
+        
+        #TensorBoard
         tensor_board_writer.add_scalar("Val/Loss", val_loss, epoch)
         tensor_board_writer.add_scalar("Val/Acc_PD1", val_acc_PD1, epoch)
         tensor_board_writer.add_scalar("Val/F1_PD1", val_f1_PD1, epoch)
@@ -195,9 +197,9 @@ def main():
             f"Train Loss: {train_loss:.4f} | Train Acc: {train_acc_PD1:.4f} | Train F1: {train_f1_PD1:.4f} | "
             f"Val Loss: {val_loss:.4f} | Val Acc: {val_acc_PD1:.4f} | Val F1: {val_f1_PD1:.4f}"
         )
-
-            
-
+        
+        
+        #Early Stopping
         early_stopping(val_f1_PD1, trace_model)
         if early_stopping.early_stop:
             print("Early stopping triggered")
@@ -205,12 +207,10 @@ def main():
 
     tensor_board_writer.close()
    
-
-
-    #Load the Best model
+    #Load the Best Checkpoint model
     early_stopping.load_best_weights(trace_model)
-
-    ## Save all the model(only to resume training)
+    
+    #Save all the model
     torch.save({
         "epoch": epoch,
         "model_state_dict": trace_model.state_dict(),
