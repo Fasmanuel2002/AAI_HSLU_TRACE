fe0e6ab745a07b6115f60e93ad9f9a29b75b6d18
ManuelFas <manuelfasruiz@gmail.com>
2026-01-08 17:40:33 +0100

    Added metric Macro F1

--- Otto_TRACE/utils/training_utils.py
+++ Otto_TRACE/utils/training_utils.py
@@ -2,19 +2,23 @@ from sklearn.metrics import f1_score
 from typing import Tuple, List
 import torch
 
-def search_best_f1_thr(val_probs, val_true, thresholds) -> Tuple[float, float]: 
+def search_best_f1_thr(val_probs, val_true, thresholds) -> Tuple[float, float, float]: 
     """
     # Convert continuous probabilities [0, 1] into binary predictions [0 or 1]
     # based on the current threshold candidate 't'
     # If this threshold results in a better F1 score, update our best values
     """
-    best_thr, best_f1 = 0.5, 0.0
+    best_thr, best_f1, best_macro_f1 = 0.5, 0.0, 0.0
     for t in thresholds:
         pred = (val_probs >= t).astype(int)
         f1 = f1_score(val_true, pred, zero_division=0)
+        macro_f1 = f1_score(val_true, pred ,zero_division=0, average="macro")
+        
         if f1 > best_f1:
-            best_f1, best_thr = f1, t
-    return float(best_f1), float(best_thr)
+            best_f1 = f1
+            best_thr = t
+            best_macro_f1 = macro_f1
+    return float(best_f1), float(best_macro_f1), float(best_thr)
 
 
 
