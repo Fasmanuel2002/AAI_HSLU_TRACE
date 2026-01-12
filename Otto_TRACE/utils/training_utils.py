9a86494231397eca5fec04c8c43ca558f28a1963
ManuelFas <manuelfasruiz@gmail.com>
2026-01-12 15:03:08 +0100

    Added new functions

--- Otto_TRACE/utils/training_utils.py
+++ Otto_TRACE/utils/training_utils.py
@@ -1,8 +1,12 @@
 from sklearn.metrics import f1_score
 from typing import Tuple, List
+
 import torch
 from torch.utils.data import DataLoader
 from torch import Tensor
+from Otto_TRACE.model.trace import TRACE
+from Otto_TRACE.dataset.otto_final import TraceOttoDataset
+from torch import device as torch_device
 
 def search_best_f1_thr(val_probs, val_true, thresholds) -> Tuple[float, float, float]: 
     """
@@ -23,15 +27,41 @@ def search_best_f1_thr(val_probs, val_true, thresholds) -> Tuple[float, float, f
     return float(best_f1), float(best_macro_f1), float(best_thr)
 
 
+def initialize_TRACE_model(dataset_processed : TraceOttoDataset, num_classes: int, device : torch_device) -> TRACE:
+    """
+    Initialize the TRACE paper from the paper part 2.3 (Model Architecture)
+    """
+    max_aid = max(
+        session[0]["aid"].max().item()
+        for session in dataset_processed
+    )
+    max_type = max(
+        session[0]["type"].max().item()
+        for session in dataset_processed
+    )
 
-def update_binary_metrics(
-    logit : torch.Tensor,
-    targets : torch.Tensor,
-    correct_predictions : int,
-    total_predictions : int,
-    y_true_list : list,
-    y_pred_list : list,
-    threshold : float = 0.5) -> Tuple[int,int]:
+    num_embeddings_aid = max_aid + 1  
+    num_embeddings_event_type = max_type + 1
+    trace_model = TRACE(
+        num_embeddings_aid=num_embeddings_aid,
+        num_embeddings_event_type=num_embeddings_event_type,
+        embedding_dim=32,
+        num_classes=num_classes
+    )  
+      
+
+    trace_model = trace_model.to(device)
+    
+    return trace_model
+
+
+def update_binary_metrics(logit : torch.Tensor,
+                          targets : torch.Tensor,
+                          correct_predictions : int,
+                          total_predictions : int,
+                          y_true_list : list,
+                          y_pred_list : list,
+                          threshold : float = 0.5) -> Tuple[int,int]:
     """
     Convert logits to binary predictions, update accuracy counters, and store
     true/predicted labels for later metric computation.
@@ -48,11 +78,10 @@ def update_binary_metrics(
     return correct_predictions, total_predictions
 
 
-def append_probs_and_true(
-    logits: torch.Tensor,
-    targets: torch.Tensor,
-    probs_list: List[torch.Tensor],
-    true_list: List[torch.Tensor]) -> None:
+def append_probs_and_true(logits: torch.Tensor,
+                          targets: torch.Tensor,
+                          probs_list: List[torch.Tensor],
+                          true_list: List[torch.Tensor]):
     """
     Appends sigmoid probabilities and targets (both moved to CPU) to lists.
     logits/targets expected shape: (B, 1) for binary task. 
@@ -62,11 +91,9 @@ def append_probs_and_true(
     true_list.append(targets.detach().cpu())
     
     
-def compute_f1_tasks(
-y_true_batches: List[torch.Tensor],
-    y_pred_batches: List[torch.Tensor],
-    zero_division: int = 0
-) -> float:
+def compute_f1_tasks(y_true_batches: List[torch.Tensor], 
+                     y_pred_batches: List[torch.Tensor],
+                     zero_division: int = 0) -> float:
     """
     Concatenates batch-level tensors and computes F1 score.
     """
@@ -76,7 +103,11 @@ y_true_batches: List[torch.Tensor],
 
 
 
-def ratio_finder_single_task(train_loader : DataLoader, task_train : str , device) -> Tuple[Tensor, Tensor]:
+def ratio_finder_single_task(train_loader : DataLoader, task_train : str , device : torch_device) -> Tuple[Tensor, Tensor]:
+    """
+    Computes the ratio between positive (1) and negative (0) samples for a single task.
+    """
+
     labels_list = []
     for _, targets in train_loader:
         labels_list.append(targets[task_train].view(-1)) #(Batch, )
@@ -100,12 +131,16 @@ def ratio_finder_single_task(train_loader : DataLoader, task_train : str , devic
     return (w_pos, w_neg)
 
 
-def ratios_finder_multi_task(train_loader : DataLoader, device) -> Tuple[Tensor, Tensor, Tensor]: 
+
+def ratios_finder_multi_task(train_loader : DataLoader, device: torch_device) -> Tuple[Tensor, Tensor, Tensor]: 
+    """
+    Computes the ratio between positive (1) and negative (0) samples for  Multi task Learning
+    """
     labels_list_ATC = []
     labels_list_SAT = []
     labels_list_MAP = []
     
-    for inputs, targets in train_loader:
+    for _, targets in train_loader:
         labels_list_ATC.append(targets["ATC"].view(-1)) #(Batch, )
         labels_list_SAT.append(targets["SAT"].view(-1)) #(Batch, )
         labels_list_MAP.append(targets["MAP"].view(-1)) # (Batch, )
@@ -140,3 +175,11 @@ def ratios_finder_multi_task(train_loader : DataLoader, device) -> Tuple[Tensor,
     w_pos_MAP = torch.tensor([ratio_MAP], device=device).float()
     
     return (w_pos_ATC, w_pos_SAT, w_pos_MAP)
+
+def concate_probs_true(val_probs : List[Tensor] , val_true : List[Tensor]) -> Tuple:
+    """
+    Concatonate the Probabilities and true labels
+    """
+    val_probs = torch.cat(val_probs).detach().cpu().numpy().ravel()
+    val_true = torch.cat(val_true).detach().cpu().numpy().ravel()
+    return val_probs, val_true
